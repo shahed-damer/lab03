49b6efdb25d7d52a063b34c39a99880adca3c247
shahedQerbaa <>
2021-05-27 17:31:58 +0000

    my file

---
+++ appjava.js
@@ -0,0 +1,21 @@
+var flo = "Carnations";
+
+
+
+
+var userName = prompt("Whats your name ?");
+alert("welcome to our websit " + userName )
+
+
+var fav kind  flower  = prompt("What is your fav kind of flower  ?");
+// console.log(userAge);
+
+if(fav kind  flower === Carnations ){
+  // console.log("what do you work ?");
+  document.write("<p> Welcome to the club </p>")
+}else if(fav kind  flower === "Gardenias"){
+  document.write('<img src="https://www.almanac.com/sites/default/files/image_nodes/gardenia-close.jpg"/>')
+}else{
+  alert('please next time answer either Carnations or Gardenias
+ ') 
+
